6539c488f2409937f564fe7a84ed8d9fcd768f9e
Xinwen Fu <69218457+xinwenfu@users.noreply.github.com>
2022-02-27 00:35:05 -0500

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,19 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
 // Load Wi-Fi library
 #include <WiFi.h>
 #include <HTTPClient.h>
+
 // Replace with your network credentials
-const char* ssid = "SSID";
-const char* password = "PASSWORD";
+const char* ssid = "CWS5Q";
+const char* password = "LN7S3S282Q7NLCD8";
+
+#define FU_HTTPS
 
 //Your Domain name with URL path or IP address with path
-//String serverName = "http://192.168.1.7/";
-String serverName = "http://192.168.1.7/test_get.php";
+String serverName = "https://192.168.1.7/test_get.php";
+
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
@@ -37,8 +39,8 @@ unsigned long timerDelay = 5000;
 // Pin 15 can work but DHT must be disconnected during program upload.
 
 // Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
-#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 // See guide for details on sensor wiring and usage:
@@ -46,6 +48,31 @@ unsigned long timerDelay = 5000;
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
+#ifdef FU_HTTPS
+const char* root_ca= \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDmzCCAoMCFEGekyA+W02NLVwtBzTiy6kLbrFHMA0GCSqGSIb3DQEBCwUAMIGJ\n" \
+"MQswCQYDVQQGEwJVUzEWMBQGA1UECAwNTWFzc2FjaHVzZXR0czEPMA0GA1UEBwwG\n" \
+"TG93ZWxsMQwwCgYDVQQKDANVTUwxCzAJBgNVBAsMAkNTMRQwEgYDVQQDDAsxOTIu\n" \
+"MTY4LjEuNzEgMB4GCSqGSIb3DQEJARYReGlud2VuX2Z1QHVtbC5lZHUwHhcNMjIw\n" \
+"MjI3MDIyNTA5WhcNMjMwMjI3MDIyNTA5WjCBiTELMAkGA1UEBhMCVVMxFjAUBgNV\n" \
+"BAgMDU1hc3NhY2h1c2V0dHMxDzANBgNVBAcMBkxvd2VsbDEMMAoGA1UECgwDVU1M\n" \
+"MQswCQYDVQQLDAJDUzEUMBIGA1UEAwwLMTkyLjE2OC4xLjcxIDAeBgkqhkiG9w0B\n" \
+"CQEWEXhpbndlbl9mdUB1bWwuZWR1MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIB\n" \
+"CgKCAQEAuOhoDm1+zc6XnyhWayfpRkxTbsSqML7OE1NkzCG1WeX686WYKEoPyObG\n" \
+"vLBPFO7X2iu3m/pWP/mruWO5+7yTFIPi/2nDRbEngbHadl/4YiABTHfw9FuHkLWL\n" \
+"1Et8h/yl1qo0waNHGFluBdsFu09rRCDZbi0o8Pkt+AsoFZSOg0zOxp7/OHs6muPP\n" \
+"+LlCMxHvgY8sh6l3wjDRd/YCJceLRB+hhBdn6rPpXK5/00jNrT8r928I9fOUVR+h\n" \
+"yDHluG8AMP/iTsi2UoTp1j9gD0WCEqwnVtzXGeeak8ef3yQ3jSwvxXMNK4qoba9W\n" \
+"YHyhAJ/v2OjCJP0CCDxEbEJj2n7dQQIDAQABMA0GCSqGSIb3DQEBCwUAA4IBAQAY\n" \
+"qGFBk4kpUJc49en/bsMBpHZHS8zVPThj8NyZ1OEtN4uLGmNdj1XERn2JE8CDtPyn\n" \
+"6Dqpy+TOJl78niYI5zreUYkBhEJfsZmr2OQXc8FP9ht3emQAqTyuxzDgb+BkQMzA\n" \
+"etAqJ1t3SQqOqVJ+6nPA06nXNgmgfsSW8XDYnxmp3bDsTiY9G2qPwO6TEhYoI5Tj\n" \
+"MzUTQh5M10gPKJHL5qlvpuEkSvh5OIjsIlftgY9bVFxqWvMLZyHwZTOHYE4QJ5PR\n" \
+"mdfcQAjZPxe0ItQiiSsrOIVqiKEKZW6/IMb28bqBLRiHsw3eKvho2gF7oR1QsB3N\n" \
+"Tgx2ehqfvmu2dxJ/+FWr\n" \
+"-----END CERTIFICATE-----\n";
+#endif
 
 void setup() {
   Serial.begin(115200);
@@ -98,10 +125,16 @@ void loop() {
       String myHumidityString(event.relative_humidity);     // empty string
 
       String serverPath = serverName + "?Temperature="+myTemperatureString+"&Humidity="+myHumidityString;
-      
-      // Your Domain name with URL path or IP address with path
+     
+      #ifdef FU_HTTP
       http.begin(serverPath.c_str());
-      
+      #endif
+
+      // Your Domain name with URL path or IP address with path
+      #ifdef FU_HTTPS
+      http.begin(serverPath.c_str(), root_ca);
+      #endif
+
       // Send HTTP GET request
       int httpResponseCode = http.GET();
       
